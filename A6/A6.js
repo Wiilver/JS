4f884516aa84f9ffa595b7cce2ae8953198cba59
Wiilver <susareywvh@gmail.com>
2026-01-28 22:55:01 -0600

    ya no es tan horrendo

--- A6/A6.js
+++ A6/A6.js
@@ -3,13 +3,91 @@ function cambiardado()
     document.getElementById("dados").style = "visibility:visible;";
 }
 
-function dadeo()
+function d100()
 {
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "100";
+    document.getElementById("dado").textContent = str;
+    document.getElementById("dados").style = "visibility:hidden;";
+}
+
+function d20()
+{
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "20";
+    document.getElementById("dado").textContent = str;
+    document.getElementById("dados").style = "visibility:hidden;";
+}
+
+function d12()
+{
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "12";
+    document.getElementById("dado").textContent = str;
+    document.getElementById("dados").style = "visibility:hidden;";
+}
+
+function d10()
+{
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "10";
+    document.getElementById("dado").textContent = str;
+    document.getElementById("dados").style = "visibility:hidden;";
+}
+
+function d8()
+{
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "8";
+    document.getElementById("dado").textContent = str;
+    document.getElementById("dados").style = "visibility:hidden;";
+}
+
+function d6()
+{
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "6";
+    document.getElementById("dado").textContent = str;
+    document.getElementById("dados").style = "visibility:hidden;";
+}
+
+function d4()
+{
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "4";
+    document.getElementById("dado").textContent = str;
+    document.getElementById("dados").style = "visibility:hidden;";
+}
+
+function d2()
+{
+    let str
+    str = document.getElementById("dado").textContent;
+    str = str.slice(0,str.indexOf(":")+1);
+    str += "2";
+    document.getElementById("dado").textContent = str;
     document.getElementById("dados").style = "visibility:hidden;";
 }
 
 function lanzardado(n)
 {
-    
-    dadeo();
+    let str = document.getElementById("dado").textContent;
+    str = str.slice(str.indexOf(":")+1,str.length);
+    let x = Math.floor((Math.random()*Number(str)+1));
+    document.getElementById("num").textContent = x;
+    console.log(str);
 }
